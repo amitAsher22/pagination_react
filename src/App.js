5a77884bbfc5ce0aabb8a1ed19f072910ac3eb63
amita <amitasher4@gmail.com>
2023-10-19 17:52:50 +0300

    add post

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import "./App.css";
+import { data_Mock } from "./data/mock_data";
+import ReactPaginage from "react-paginate";
+
+function App() {
+  const [data, setData] = useState(data_Mock.slice(0, 50));
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const usersPerPage = 10;
+  const pagesVisited = pageNumber * usersPerPage;
+
+  const displayUsers = data
+    .slice(pagesVisited, pagesVisited + usersPerPage)
+    .map((user) => {
+      return (
+        <div className="users">
+          <h2>{user.firstName}</h2>
+          <p>{user.lastName}</p>
+          <p>{user.Email}</p>
+        </div>
+      );
+    });
+
+  const pageCount = Math.ceil(data.length / usersPerPage);
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
+
+  return (
+    <div className="App">
+      {displayUsers}
+      <ReactPaginage
+        previousLabel={"Previous"}
+        nextLabel={"Next"}
+        pageCount={pageCount}
+        onPageChange={changePage}
+        containerClassName={"paginationBttns"}
+        previousLinkClassName={"paginationsBttns"}
+        nextLinkClassName={"nextBttn"}
+        disabledClassName={"paginationDisabled"}
+        activeClassName={"paginationActive"}
+      />
+    </div>
+  );
+}
+
+export default App;
